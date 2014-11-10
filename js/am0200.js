e8b03d8b399c67918c6b626a9d2e167823c9e79e
LoeiFy <loeify@gmail.com>
2014-11-10 22:31:05 +0800

    popstate & mousescroll

--- js/am0200.js
+++ js/am0200.js
@@ -43,18 +43,20 @@ var hometitle;
 var currentstate = [],              // current url title
     historystates = new Array;      // history states
 
+var urlpath = location.pathname;    // if to load more
+
 $(function($) {
 
     $(document).on('keydown', function(e) {
         switch (e.keyCode) {
 
             case 40:    // down
-                if (url) toLoad();
+                if (url && urlpath == '/') toLoad();
                 if (position < totalpost) {
                     position ++;
                     sectionMove(position * window.innerHeight)
 
-                    history.pushState({ url: historystates[position][0], title: historystates[position][1] }, historystates[position][1], historystates[position][0])
+                    history.pushState({ url: historystates[position][0], title: historystates[position][1], position: position }, historystates[position][1], historystates[position][0])
                     document.title = historystates[position][1];
 
                 } else {
@@ -72,7 +74,7 @@ $(function($) {
                     position --;
                     sectionMove(position * window.innerHeight)
 
-                    history.pushState({ url: historystates[position][0], title: historystates[position][1] }, historystates[position][1], historystates[position][0])
+                    history.pushState({ url: historystates[position][0], title: historystates[position][1], position: position }, historystates[position][1], historystates[position][0])
                     document.title = historystates[position][1];
                 } else {
                     $('#top').height(0)     /* need to define 0px */
@@ -88,6 +90,10 @@ $(function($) {
             case 37:    // left
             break;
 
+            case 9:     // tab
+                return false;
+            break;
+
         }
     })
 
@@ -97,12 +103,12 @@ $(function($) {
     hometitle = document.title.indexOf(' ') != -1 ? document.title.split(' ')[0] : document.title;
     currentstate.url = $('.post').find('.entry').attr('href');
     currentstate.title = hometitle +' - '+ $('.post').find('.entry').attr('title');
-    history.replaceState({ url: currentstate.url, title: currentstate.title }, currentstate.title, currentstate.url)
+    history.replaceState({ url: currentstate.url, title: currentstate.title, position: 0 }, currentstate.title, currentstate.url)
     document.title = currentstate.title;
 
     historystates.push([currentstate.url, currentstate.title])
 
-    if (url) toLoad();
+    if (url && urlpath == '/') toLoad();
     var num = 0;
     function toLoad() {
         
@@ -128,5 +134,65 @@ $(function($) {
             if (url) toLoad();
         })
     }
+
+    window.addEventListener('popstate', function(e) {
+        var states = e.state;
+
+        if (!states) return;
+
+        if (urlpath != '/') location.href = '/';
+
+        document.title = states.title;
+
+        sectionMove(states.position * window.innerHeight)
+    })
+
+    var scrollmark = true;
+    $('body').on('mousewheel DOMMouseScroll', function(e) {
+        e.preventDefault()
+        var data = e.originalEvent.wheelDelta || e.originalEvent.detail * -1;
+
+        var time = 500;
+        if (navigator.platform == 'MacIntel' || navigator.platform == 'MacPPC') time = 1500;
+
+        if (scrollmark) {
+            scrollmark = false;
+
+            if (data < 0) {     // down
+                if (url && urlpath == '/') toLoad();
+                if (position < totalpost) {
+                    position ++;
+                    sectionMove(position * window.innerHeight)
+
+                    history.pushState({ url: historystates[position][0], title: historystates[position][1], position: position }, historystates[position][1], historystates[position][0])
+                    document.title = historystates[position][1];
+
+                } else {
+                    $('#bottom').height(70)
+                    sectionBottom(position * window.innerHeight + 30, function() {
+                        sectionBottom(position * window.innerHeight, function() {
+                            $('#bottom').height(0)
+                        })
+                    })
+                }
+            }
+            if (data > 0) {     // up
+                if (position >= 1) { 
+                    position --;
+                    sectionMove(position * window.innerHeight)
+
+                    history.pushState({ url: historystates[position][0], title: historystates[position][1], position: position }, historystates[position][1], historystates[position][0])
+                    document.title = historystates[position][1];
+                } else {
+                    $('#top').height(0)     /* need to define 0px */
+                    $('#top').animate({'height': 30}, 250, 'easeInOutQuad', function() {
+                        $('#top').animate({'height': 0}, 250, 'easeInOutQuad')
+                    })
+                }
+            }
+
+            setTimeout(function() {scrollmark = true}, time)
+        }
+    })
 
 })
